4b123bca3646ca0d8074c0d38f65f7b13658b2a3
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-07-15 05:42:31 +0700

    Them phan xoa

--- src/main/java/com/c0220i1/group/controller/ProductController.java
+++ src/main/java/com/c0220i1/group/controller/ProductController.java
@@ -9,10 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
@@ -54,4 +51,51 @@ public class ProductController {
         modelAndView.addObject("message","Thêm Thành Công !");
         return modelAndView;
     }
+
+    @GetMapping("/showList")
+    public String showList(@RequestParam("s")Optional<String>s, @PageableDefault(size = 20) Pageable pageable, Model model){
+        Page<Product> products;
+        if (s.isPresent()){
+            products =productService.findAllByNameContaining(s.get(),pageable);
+        }else {
+            products =productService.findAll(pageable);
+        }
+        model.addAttribute("products",products);
+        return "listProduct";
+    }
+
+
+    @GetMapping("/edit-product/{id}")
+    public ModelAndView showEditForm(@PathVariable("id")Long id){
+        Product product = productService.findById(id).orElse(null);
+        ModelAndView modelAndView = new ModelAndView("editProduct");
+        modelAndView.addObject("product",product);
+        modelAndView.addObject("categories",categoryService.findAll());
+        return modelAndView;
+    }
+
+    @PostMapping("/edit-product")
+    public ModelAndView updateProduct(@ModelAttribute("product")Product product){
+        productService.save(product);
+        ModelAndView modelAndView = new ModelAndView("editProduct");
+        modelAndView.addObject("product",product);
+        modelAndView.addObject("message","Thay Đổi Thông Tin Thành Công !");
+        return modelAndView;
+    }
+    @GetMapping("/delete-product/{id}")
+    public ModelAndView showDeleteForm(@PathVariable("id")Long id){
+        Product product = productService.findById(id).orElse(null);
+            ModelAndView modelAndView = new ModelAndView("deleteProduct");
+            modelAndView.addObject("product",product);
+            return modelAndView;
+
+    }
+
+    @PostMapping("/delete-product")
+    public String deleteProduct(@ModelAttribute("product")Product product){
+        productService.remove(product.getId());
+        return "redirect:/showList";
+    }
+
+
 }
