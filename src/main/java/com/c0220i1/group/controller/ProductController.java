69c86ad015b908c5c98ac84b40786810f3519628
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-07-14 15:36:26 +0700

    Them phan view admin

--- src/main/java/com/c0220i1/group/controller/ProductController.java
+++ src/main/java/com/c0220i1/group/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,4 +23,16 @@ public class ProductController {
     private ProductService productService;
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping("/admin")
+    public String viewPage(@RequestParam("s")Optional<String>s, @PageableDefault(size = 9) Pageable pageable, Model model){
+        Page<Product> products;
+        if (s.isPresent()){
+            products =productService.findAllByNameContaining(s.get(),pageable);
+        }else {
+            products =productService.findAll(pageable);
+        }
+        model.addAttribute("products",products);
+        return "viewAdmin";
+    }
 }
