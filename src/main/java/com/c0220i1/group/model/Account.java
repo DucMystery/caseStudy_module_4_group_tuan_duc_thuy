54bb8ed98930871afba1af7e817b59a9f4f90f90
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-07-15 14:48:02 +0700

    dong nhat 3 may

--- src/main/java/com/c0220i1/group/model/Account.java
+++ src/main/java/com/c0220i1/group/model/Account.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 public class Account {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false, unique = true)
