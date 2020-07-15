c7023d7d74ebea8cd22789b74429234b55522cfe
tathithuyvn89 <61693986+tathithuyvn89@users.noreply.github.com>
2020-07-15 14:53:42 +0700

    Add order and orderDetail

---
+++ src/main/java/com/c0220i1/group/controller/OrderController.java
@@ -0,0 +1,8 @@
+package com.c0220i1.group.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+  public class OrderController {
+
+}
---
+++ src/main/java/com/c0220i1/group/model/OrderDetail.java
@@ -0,0 +1,17 @@
+package com.c0220i1.group.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+  public class OrderDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int quantity;
+    private double amount;
+    @ManyToOne
+    private Product product;
+}
